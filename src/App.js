7a8d3a9ff47814c7a6c16a33b53a1bebdb19dd07
asmbatha <asmbatha.13@gmail.com>
2017-09-15 14:58:42 +0200

    update App.js
    update game to use moves count rather then constantly updating X/O

--- src/App.js
+++ src/App.js
@@ -10,38 +10,38 @@ class App extends Component {
 
     componentWillMount() {
         this.setState({
-            currP: 'X',
-            board: ['', '', '', '', '', '', '', '', ''],
+            moves: 0,
+            board: [{value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}],
         })
     }
     
     handlePlay(i) {
         let board = this.state.board;
-        if(board[i] !== '') return
-        board[i] = this.state.currP;
+        if(board[i].value !== '') return;
         
-        let toggleSym = () => this.state.currP === 'X' ? 'O' : 'X'
+        let getSym = () => this.state.moves % 2 === 0 ? 'O' : 'X'
 
-        let check = ({currP, board}) => {
-            return [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]].find(s=> board[s[0]] === toggleSym() && board[s[0]] === board[s[1]] && board[s[0]] === board[s[2]])
+        let check = ({board}) => {
+            return [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]].find(s=> board[s[0]].value === getSym() && board[s[0]].value === board[s[1]].value && board[s[0]].value === board[s[2]].value)
         }
-
+        
+        this.state.moves++;
+        board[i] = getSym();
         this.setState({
-            currP: toggleSym(),
             board: board,
         },()=>{
             setTimeout(() => {
                 if(check(this.state)){
-                    alert(`Congratulations. '${toggleSym()}' won the game`)
+                    alert(`Congratulations. '${getSym()}' won the game`)
                     this.setState({
-                        currP: 'X',
-                        board: ['','','','','','','','',''],
+                        moves: 0,
+                        board: [{value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}],
                     })
-                }else if(!(this.state.board.filter(x=>x==='').length)){
+                }else if(this.state.moves === 9){
                     alert(`Draw`)
                     this.setState({
-                        currP: 'X',
-                        board: ['','','','','','','','',''],
+                        moves: 0,
+                        board: [{value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}, {value:''}],
                     })
                 }
             },0);
